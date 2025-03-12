12e32aea2f12487e547a53f4b2a5b247001afbf9
Deepak B <deepakbhangale1996@gmail.com>
2025-03-12 22:50:36 +0530

    handled condition for negative numbers and multiple numbers

--- src/utility/CalculateString.js
+++ src/utility/CalculateString.js
@@ -3,6 +3,19 @@
 const CalculateString = (numbers) =>{
     if (!numbers.trim()) return 0; // Handle empty input
 
+    let delimiter = /,/; // Default delimiters (comma and newline)
+
+    const numArray = numbers
+    .split(delimiter)
+    .map((n) => n.trim())
+    .filter((n) => /^[0-9-]+$/.test(n))
+    .map((n) => parseInt(n, 10));
+    
+    const negatives = numArray.filter((n) => n < 0);
+    if (negatives.length > 0) {
+      throw new Error(`Negative numbers not allowed: ${negatives.join(", ")}`);
+    }
+  
 }
 
 export default CalculateString;
--- src/utility/CalculateString.test.js
+++ src/utility/CalculateString.test.js
@@ -4,4 +4,12 @@ describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
     expect(CalculateString("")).toBe(0);
   });
+
+  test("should throw error if negative numbers are included", () => {
+    expect(() => CalculateString("1,-2,3")).toThrow("Negative numbers not allowed: -2");
+  });
+
+  test("should throw error if negative numbers are included", () => {
+    expect(() => CalculateString("1,-2,3,-8")).toThrow("Negative numbers not allowed: -2, -8");
+  });
 });
